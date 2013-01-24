a8717262f58878b53dcf9ef69da866548abec1a0
vanetix <matmcfarland@gmail.com>
2013-01-23 19:09:02 -0600

    update tests for added functionality

--- test/expected/bad.js
+++ test/expected/bad.js
@@ -1,3 +1,3 @@
 this["Templates"] = this["Templates"] || {};
 
-this["Templates"]["test/fixtures/it's-a-bad-name.html"] = function(c,p,i){var _=this;_.b(i=i||"");_.b("Gonna have a bad time compiling.");return _.fl();;};
+this["Templates"]["test/fixtures/it's-a-bad-name.html"] = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");_.b("Gonna have a bad time compiling.");return _.fl();;});
