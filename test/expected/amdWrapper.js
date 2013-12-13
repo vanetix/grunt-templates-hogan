6651a1c26274bd7680ee92f8641e9115ef338c37
Matt McFarland <matmcfarland@gmail.com>
2013-12-12 20:56:04 -0600

    update tests for new spacing

--- test/expected/amdWrapper.js
+++ test/expected/amdWrapper.js
@@ -1,8 +1,5 @@
 define(function() {
-
 this["Templates"] = this["Templates"] || {};
-
 this["Templates"]["test/fixtures/template.html"] = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");_.b("<article>");_.b("\n" + i);_.b("  <h1>");_.b(_.v(_.f("title",c,p,0)));_.b("</h1>");_.b("\n" + i);_.b("  <p>");_.b(_.v(_.f("content",c,p,0)));_.b("</p>");_.b("\n" + i);_.b("</article>");return _.fl();;});
-
-  return this["Templates"];
+return this["Templates"];
 });
